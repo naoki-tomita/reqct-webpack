f0e5586e6d910ccbcdceda3cb1cc39406351ac13
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2017-09-10 09:22:00 +0900

    Fiest commit.

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import * as webpack from "webpack";
+import * as path from "path";
+
+const config: webpack.Configuration = {
+  entry: "./src/script/apps/index.tsx",
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  }
+};
+
+export default config;
